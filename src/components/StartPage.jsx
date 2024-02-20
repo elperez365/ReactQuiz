85b17499fd632b40e9875b734bef249ced7534bb
Vasco Ceti <vasco.ceti@gmail.com>
2024-02-20 15:16:14 +0100

    nquestion

--- src/components/StartPage.jsx
+++ src/components/StartPage.jsx
@@ -1,8 +1,12 @@
+import { shuffledQuestions } from "../questions";
+
 export default function StartPage({ onStart }) {
+  const nQuestions = shuffledQuestions.length;
+
   return (
     <div id="quiz" className="startPage">
       <h1>Welcome to the Quiz</h1>
-      <p>You will be presented with 7 True or False questions.</p>
+      <p>You will be presented with {nQuestions} questions.</p>
       <p>Can you score 100% ?</p>
       <button id="startButton" onClick={() => onStart()}>
         Start
